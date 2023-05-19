a71df13ca3e1a1c843dc45b14b63752fc4a76e36
biancagit-606 <biancafiedalan31@yahoo.com>
2023-05-19 18:59:58 +1200

    fixed index.html bug

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ all_blogs = [blog for blog in blog_data]
 
 current_year = dt.datetime.now().year
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
 @app.route('/')
 def home():
